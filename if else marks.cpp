1c2600d014fbb0cba0638a19ed2c7333f3734445
Tribhuvan Singh Parihar <122529302+parihar5620@users.noreply.github.com>
2023-11-25 20:34:37 +0530

    Add files via upload

---
+++ if else marks.cpp
@@ -0,0 +1,20 @@
+#include
+<iostream>
+
+int main()
+{
+    int marks 
+     cout<<"Enter your marks:";
+     cin>>marks;
+
+     if(marks<<33)
+     {
+        cout<<"u are pass";
+     }
+     else
+     {
+        cout<<"u are fail";
+     }
+
+
+}
