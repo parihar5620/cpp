5bee000204cb7641572af912952e753132a48fb8
Tribhuvan Singh Parihar <122529302+parihar5620@users.noreply.github.com>
2023-11-27 21:36:44 +0530

    Add files via upload

---
+++ differencebetweentwono.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a, b, d;
+cout<<"enter first and second numbers";
+
+cin>>a>>b;
+d = a-b;
+if(d<0);
+d=d*(-1);
+
+cout<<"answer is"<<d;
+
+return 0;
+
+}
