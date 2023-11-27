4525eaf303141bdb40ebbbbf21f6652bc2aa8f17
Tribhuvan Singh Parihar <122529302+parihar5620@users.noreply.github.com>
2023-11-27 21:27:34 +0530

    Add files via upload

---
+++ printaninteger.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int number;
+
+    // Ask the user to enter an integer
+    std::cout << "Enter an integer: ";
+    
+    // Read the integer from the user
+    std::cin >> number;
+
+    // Print the entered integer
+    std::cout << "You entered: " << number << std::endl;
+
+    return 0;
+}
