938b83c4b7dc1b807368fc6dd16ae8f1f3f83453
Tribhuvan Singh Parihar <122529302+parihar5620@users.noreply.github.com>
2023-12-17 20:20:20 +0530

    Add files via upload

---
+++ armstrongnum01.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+
+int n, c=0, r, sum=0;
+
+while(n>0)
+{
+
+}
+
+return 0;
+}
---
+++ generatenumericdigit.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ for( int x = 0, x <= 10, x++)
+ {
+    cout<<x<<endl;
+ }
+return 0;
+}
+// to calculate the multiplication of reverse of number and number of digit
